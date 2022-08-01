f4a0be5137406a31a622ad159b7c166b1b82473a
Sankararaman Vedagiri <shnkar007@hotmail.com>
2022-08-01 13:27:21 +0530

    Added SOP

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,7 +12,7 @@ public class App
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
-        System.out.println("message delivered");
+        System.out.println("messagedelivered");
     }
 
     private final String getMessage() {
