7a77e22b888e6f4252a56bee0de55d1464d4475d
Sankararaman Vedagiri <shnkar007@hotmail.com>
2022-08-19 16:00:31 +0530

    New Run

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -36,7 +36,7 @@ public class AppTest
         try {
 
             // assertTrue("GitHubActions123" + System.getProperty("line.separator"), outContent.toString());
-               assertTrue("GitHubActions123"==outContent.toString());
+               assertTrue("GitHubActions fail"==outContent.toString());
         } catch (AssertionError e) {
             fail("\"message\" is not \"Hello World!\"");
         }
