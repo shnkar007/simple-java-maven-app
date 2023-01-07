dd99b390a700a6a5d6461c58b6cdc7a2c060a577
Sankararaman Vedagiri <shnkar007@hotmail.com>
2023-01-07 10:32:47 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -36,7 +36,7 @@ public class AppTest
         try {
 
             // assertTrue("GitHubActions123" + System.getProperty("line.separator"), outContent.toString());
-               assertTrue("GitHubActions fail",true);
+               assertTrue("GitHubActions fail RKFI",true);
         } catch (AssertionError e) {
             fail("\"message\" is not \"Hello World! application\"");
         }
